3e521ebcb937d236541a90a0a5797dc9cafb83ac
Phillip <phillipables1@gmail.com>
2018-02-11 18:13:54 -0500

    empty tic tac toe project with GameEngine class and README description

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # android-simple-tic-tac-toe
 simple tic tac toe game for android
+
+https://www.youtube.com/watch?v=Atec_6EUKns
---
+++ app/src/main/java/com/example/techtron/tictactoe/GameEngine.java
@@ -0,0 +1,8 @@
+package com.example.techtron.tictactoe;
+
+/**
+ * Created by user on 2/11/2018.
+ */
+
+public class GameEngine {
+}
