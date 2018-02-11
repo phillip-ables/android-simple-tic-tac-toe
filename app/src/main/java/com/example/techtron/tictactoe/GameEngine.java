a73265ed10eb0cb9cae5bf07f64594681e9e333e
Phillip <phillipables1@gmail.com>
2018-02-11 18:45:17 -0500

    Game engine coded

--- app/src/main/java/com/example/techtron/tictactoe/GameEngine.java
+++ app/src/main/java/com/example/techtron/tictactoe/GameEngine.java
@@ -1,8 +1,90 @@
 package com.example.techtron.tictactoe;
 
+import java.util.Random;
+
 /**
  * Created by user on 2/11/2018.
  */
 
 public class GameEngine {
+    //random object that will be used for the computer
+    private static final Random RANDOM = new Random();
+    //element of the ball will be stored in a one demention array of char
+    private char[] elts;
+    private char currentPlayer;
+    private boolean ended;
+
+    public GameEngine() {
+        elts = new char[9];
+        newGame();
+    }
+
+    public boolean isEnded() {
+        return ended;
+    }
+
+    public char play(int x, int y) {
+        if(!ended && elts[3 * y + x] == ' ') {
+            elts[3 * y + x] = currentPlayer;
+            changePlayer();
+        }
+
+        return checkEnd();
+    }
+
+    public void changePlayer() {
+        currentPlayer = (currentPlayer == 'X' ? '0' : 'X');
+    }
+
+    public char elt(int x, int y) {
+        return elts[3 * y + x];
+    }
+
+    public void newGame() {
+        for (int i = 0; i < elts.length; i++) {
+            elts[i] = ' ';
+        }
+        currentPlayer = 'X';
+        ended = false;
+    }
+
+    public char checkEnd() {
+        for (int i = 0; i <3; i++) {
+            if (elt(i, 0) != ' ' && elt(i, 0) == elt(i, 1) && elt(i, 1) == elt(i, 2)) {
+                ended = true;
+                return elt(i, 0);
+            }
+            if (elt(0, i) != ' ' && elt(0, i) == elt(1, i) && elt(1, i) == elt(2, i)) {
+                ended = true;
+                return elt(0, i);
+            }
+        }
+        if(elt(0, 0) != ' ' && elt(0, 0) == elt(1,1) && elt(1,1) == elt(2,2)) {
+            ended = true;
+            return elt(0,0);
+        }
+        if (elt(2,0) !=' ' && elt(2,0) == elt(1,1) && elt(1,1) == elt(0,2)) {
+            ended = true;
+            return elt(2,0);
+        }
+        for (int i = 0; i < 9; i++) {
+            if (elts[i] == ' '){
+                return ' ';
+            }
+        }
+        return 'T'; //Tie
+    }
+
+    public char computer() {
+        if(!ended) {
+            int position = -1;
+            do {
+                position = RANDOM.nextInt(9);
+            } while (elts[position] != ' ');
+
+            elts[position] = currentPlayer;
+            changePlayer();
+        }
+        return checkEnd();
+    }
 }
