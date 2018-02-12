6e8f309f3fe05017bf855345fb2adf31688a1443
Phillip <phillipables1@gmail.com>
2018-02-11 20:31:22 -0500

    implement the view class and extend the constructor for Board View class

---
+++ app/src/main/java/com/example/techtron/tictactoe/BoardView.java
@@ -0,0 +1,20 @@
+package com.example.techtron.tictactoe;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by user on 2/11/2018.
+ */
+
+public class BoardView extends View {
+    public BoardView(Context context) {
+        super(context);
+    }
+
+    public BoardView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
